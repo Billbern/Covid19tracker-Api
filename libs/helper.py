4b02af242469467da4f2d52934ddcc3f139352f2
Billbern <denis.abaidoo@gmail.com>
2020-08-11 02:42:55 +0000

    added a check for the date

--- libs/helper.py
+++ libs/helper.py
@@ -14,8 +14,12 @@ def getDate(number_of_days=1):
         
         :param number_of_days defaults to 1 but can take any number of days
     """
+    check_today = datetime.datetime.now().replace(hour=4,minute=0,second=0,microsecond=0)
     today = datetime.datetime.today()
-    yesterday = today - datetime.timedelta(days=number_of_days)
+    if today < check_today:
+        yesterday = today - datetime.timedelta(days=number_of_days+1)
+    else:
+        yesterday = today - datetime.timedelta(days=number_of_days)
     return yesterday.strftime('%m-%d-%Y')
 
 
@@ -28,5 +32,5 @@ def fetch_data(url, date=None):
     
     for items in actual_data:
         if items[3] in countries:
-            data.append({"id": len(data),"Country" : items[3], "Confirmed" : items[7], "Deaths": items[8], "Recovered": items[9], "Active": items[10]})
+            data.append({"id": len(data)+1,"Country" : items[3], "Confirmed" : items[7], "Deaths": items[8], "Recovered": items[9], "Active": items[10]})
     return data
